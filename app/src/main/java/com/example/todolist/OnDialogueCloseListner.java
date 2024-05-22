77be165bb1169247ab9b5637dcc46db05cc4060f
Ronnin-01 <omarmd425@gmail.com>
2024-05-22 20:12:10 +0530

    Added new repo to do list

---
+++ app/src/main/java/com/example/todolist/OnDialogueCloseListner.java
@@ -0,0 +1,8 @@
+package com.example.todolist;
+
+import android.content.DialogInterface;
+
+public interface OnDialogueCloseListner {
+
+        void onDialogueClose(DialogInterface dialogInterface);
+}
